d3c54ad722ebc5689469baf0191bded40ed554e1
Rob Deans <17593196+rob-deans@users.noreply.github.com>
2026-03-22 22:21:24 +0000

    Load queue from WAL when creating queue
    Also, create new append function to avoid truncating all the time

--- src/models/persistent_queue.rs
+++ src/models/persistent_queue.rs
@@ -1,34 +1,35 @@
 use log::{debug, error, info};
-use std::io::Write;
+use std::fs;
+use std::io::{BufRead, BufReader, Write};
 use std::{collections::VecDeque, fs::OpenOptions};
 
 use crate::models::owntracks::OwntracksPayload;
 
 #[derive(Debug, Clone)]
 pub struct PersistentQueue {
-    filepath: String,
+    pub filepath: String,
     pub queue: VecDeque<OwntracksPayload>,
 }
 
 impl PersistentQueue {
     pub fn new(filepath: String) -> Self {
+        let data = load_wal(&filepath);
+
         Self {
             filepath,
-            queue: VecDeque::new(),
+            queue: data,
         }
     }
 
     pub fn push(&mut self, payload: OwntracksPayload) {
+        self.append(&payload);
         self.queue.push_back(payload);
-
-        // Append to disk
-        self.save();
+        debug!("Queue size {}", self.queue.len());
     }
 
     pub fn pop(&mut self) -> Option<OwntracksPayload> {
         let payload = self.queue.pop_front();
         self.save();
-        // write to disk
         payload
     }
 
@@ -36,9 +37,26 @@ impl PersistentQueue {
         self.queue.len() == 0
     }
 
-    fn save(&mut self) {
+    fn append(&self, payload: &OwntracksPayload) {
         let mut file = OpenOptions::new()
             .create(true)
+            .append(true)
+            .open(&self.filepath)
+            .expect("This should open!");
+
+        if let Ok(json) = serde_json::to_string(&payload) {
+            if let Err(e) = writeln!(file, "{json}") {
+                error!("Couldn't write to file {}", e);
+            }
+        } else {
+            error!("Failed to deserialize payload: {payload:?}")
+        }
+    }
+
+    fn save(&self) {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
             .truncate(true)
             .open(&self.filepath)
             .expect("This should open!");
@@ -58,11 +76,43 @@ impl PersistentQueue {
     }
 }
 
-impl IntoIterator for PersistentQueue {
-    type Item = OwntracksPayload;
-    type IntoIter = std::collections::vec_deque::IntoIter<OwntracksPayload>;
+pub fn load_wal(wal_path: &String) -> VecDeque<OwntracksPayload> {
+    let file = fs::File::open(wal_path);
 
-    fn into_iter(self) -> Self::IntoIter {
-        self.queue.into_iter()
+    let mut payloads = VecDeque::new();
+
+    match file {
+        Ok(c) => {
+            info!("Loading WAL to memory from {wal_path}");
+            let reader = BufReader::new(c);
+
+            for line in reader.lines() {
+                match line {
+                    Ok(payload) => {
+                        match serde_json::from_str::<OwntracksPayload>(&payload) {
+                            Ok(data) => payloads.push_back(data),
+                            Err(error) => {
+                                panic!("Failed to serialise checkpointed responses {error}")
+                            }
+                        };
+                    }
+                    Err(e) => {
+                        error!("Failed to load payload: {e}")
+                    }
+                }
+            }
+
+            payloads
+        }
+        // TODO: Should handle all errors
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                info!("no existing WAL found");
+                payloads
+            }
+            _ => {
+                panic!("something else: {err}")
+            }
+        },
     }
 }
